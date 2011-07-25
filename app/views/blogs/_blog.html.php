fc9ba5503f1ec27b30e4795e21e39a891810705b
Martin Gouws <martin@whitewallweb.com>
2011-07-25 14:09:10 +0200

    default db

--- app/views/blogs/_blog.html.php
+++ app/views/blogs/_blog.html.php
@@ -1,23 +1,23 @@
 <a href='<?=blog_path($blog)?>' class='item-link'>
-					<div class='item'>
-						<div class='image'></div>
-						<div class='title'>
-							<h3>
-								<?=$blog->name?>
-							</h3>
-							<div class='cb'></div>	
-							<div class="info">
-								<ul>
-								
-                                                                    <li>Tags : <?=implode(",",array_map(function($tag){return $tag->name;},$blog->tags))?>  </li>
-									
-								</ul>
-							</div>
-						</div>
-						<div>
-							
-						</div>
-						<div class='grey-arrow'></div>
-						<div class='cb'></div>	
-					</div>
-				</a>
+    <div class='item'>
+        <div class='image'></div>
+        <div class='title'>
+                <h3>
+                        <?=$blog->name?>
+                </h3>
+                <div class='cb'></div>
+                <div class="info">
+                        <ul>
+
+                            <li>Tags : <?=implode(",",array_map(function($tag){return $tag->name;},$blog->tags))?>  </li>
+
+                        </ul>
+                </div>
+        </div>
+        <div>
+
+        </div>
+        <div class='grey-arrow'></div>
+        <div class='cb'></div>
+    </div>
+</a>
